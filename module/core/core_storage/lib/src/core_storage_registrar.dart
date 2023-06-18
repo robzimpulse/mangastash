eb2fcf4c7da24e232ffc20593463e158fafcd89e
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2023-06-18 20:45:01 +0700

    fix storage

--- module/core/core_storage/lib/src/core_storage_registrar.dart
+++ module/core/core_storage/lib/src/core_storage_registrar.dart
@@ -2,11 +2,13 @@ import 'package:service_locator/service_locator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'shared_preferences_storage.dart';
+import 'storage.dart';
 
 class CoreStorageRegistrar extends Registrar {
   @override
   Future<void> register(ServiceLocator locator) async {
     final local = await SharedPreferences.getInstance();
     locator.registerSingleton(SharedPreferencesStorage(local));
+    locator.alias<Storage, SharedPreferencesStorage>();
   }
 }
