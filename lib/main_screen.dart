1542e72ee82c71b213139c329fedc50d30b9adb3
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2023-06-22 08:52:44 +0700

    add child expanded

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -88,6 +88,7 @@ class _MainScreenState extends State<MainScreen> {
           onDestinationSelected: (index) => context.go(_route(index)),
           selectedIndex: index,
         ),
+        Expanded(child: widget.child),
       ],
     );
   }
