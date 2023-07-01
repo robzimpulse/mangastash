44f6e0f2a24dd19a779cf6335e7c1998c4e84196
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2023-07-01 19:14:10 +0700

    add proxy ip

--- module/library/manga_dex_api/lib/src/adapter/dio_adapter_mobile.dart
+++ module/library/manga_dex_api/lib/src/adapter/dio_adapter_mobile.dart
@@ -1,4 +1,18 @@
 import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 
-HttpClientAdapter getAdapter() => DefaultHttpClientAdapter();
+HttpClientAdapter getAdapter() {
+  final adapter = DefaultHttpClientAdapter();
+
+  const charlesIp = String.fromEnvironment('PROXY_IP', defaultValue: "");
+
+  if (charlesIp.isEmpty) return adapter;
+
+  adapter.onHttpClientCreate = (client) {
+    client.findProxy = (uri) => "PROXY $charlesIp:8888;";
+    client.badCertificateCallback = (cert, host, port) => true;
+    return client;
+  };
+
+  return adapter;
+}
