f9b38459b5c4820c84a18f0f8a90d53d20afd978
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2023-07-02 13:15:24 +0700

    add push for result

--- module/core/core_route/lib/core_route.dart
+++ module/core/core_route/lib/core_route.dart
@@ -2,5 +2,8 @@ library core_route;
 
 export 'package:go_router/go_router.dart';
 
+export 'src/base_route_args.dart';
 export 'src/base_route_builder.dart';
+export 'src/navigation_result_mixin.dart';
+export 'src/push_for_result_extension.dart';
 export 'src/route_observer.dart';
---
+++ module/core/core_route/lib/src/push_for_result_extension.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+
+import 'base_route_args.dart';
+import 'navigation_result_mixin.dart';
+
+extension PushForResult on BuildContext {
+  /// Push a page with args callback as an extra.
+  void pushForResult<T>(
+    String location, {
+    Object? args,
+    required ResultCallback<T>? callback,
+  }) => push(location, extra: BaseRouteArgs<T>(args: args, callback: callback));
+
+}
