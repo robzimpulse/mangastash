aece47be6e6034496142afb591110bbd0d1fcd6b
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2023-07-09 13:38:03 +0700

    add persistent theme

--- module/core/core_environment/lib/src/core_environment_registrar.dart
+++ module/core/core_environment/lib/src/core_environment_registrar.dart
@@ -7,7 +7,7 @@ import 'use_case/update_theme_use_case.dart';
 class CoreEnvironmentRegistrar extends Registrar {
   @override
   Future<void> register(ServiceLocator locator) async {
-    locator.registerSingleton(ThemeManager());
+    locator.registerSingleton(ThemeManager(storage: locator()));
     locator.alias<UpdateThemeUseCase, ThemeManager>();
     locator.alias<ListenThemeUseCase, ThemeManager>();
   }
--- module/core/core_environment/lib/src/manager/theme_manager.dart
+++ module/core/core_environment/lib/src/manager/theme_manager.dart
@@ -1,3 +1,4 @@
+import 'package:core_storage/core_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -5,15 +6,24 @@ import '../use_case/listen_theme_use_case.dart';
 import '../use_case/update_theme_use_case.dart';
 
 class ThemeManager implements UpdateThemeUseCase, ListenThemeUseCase {
+  final Storage _storage;
 
   final _themeDataStream = BehaviorSubject<ThemeData>.seeded(ThemeData.light());
 
+  static const _key = 'is_dark_mode';
+
+  ThemeManager({required Storage storage}) : _storage = storage {
+    final value = storage.getBool(_key);
+    if (value == null) return;
+    updateTheme(theme: value ? ThemeData.dark() : ThemeData.light());
+  }
+
   @override
   void updateTheme({required ThemeData theme}) {
+    _storage.setBool(_key, theme.brightness == Brightness.dark);
     _themeDataStream.add(theme);
   }
 
   @override
   ValueStream<ThemeData> get themeDataStream => _themeDataStream.stream;
-
 }
