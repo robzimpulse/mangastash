a421d3876b13bdef3972c6a555a7ca8b0262ed4f
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2023-09-08 09:15:42 +0700

    add widget extension

--- module/ui/ui_common/lib/ui_common.dart
+++ module/ui/ui_common/lib/ui_common.dart
@@ -13,6 +13,7 @@ export 'src/extension/bottom_sheet_extension.dart';
 export 'src/extension/expansion_tile_extension.dart';
 export 'src/extension/snackbar_extension.dart';
 export 'src/extension/string_casing_extension.dart';
+export 'src/extension/widget_extension.dart';
 export 'src/scaffold_screen.dart';
 export 'src/util/debounce.dart';
 export 'src/widget/adaptive_physic_list_view.dart';
