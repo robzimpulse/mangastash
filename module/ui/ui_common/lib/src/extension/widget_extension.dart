8f436631af28e863e4640f4b68bbe0ed01228275
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2023-09-08 09:15:36 +0700

    add widget extension

---
+++ module/ui/ui_common/lib/src/extension/widget_extension.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+extension WidgetExtension on Widget {
+  PreferredSize preferredSize({required BuildContext context, required Size size}) {
+    return PreferredSize(
+      preferredSize: size,
+      child: Container(
+        width: size.width,
+        height: size.height,
+        color: Theme.of(context).appBarTheme.backgroundColor,
+        padding: const EdgeInsets.all(8),
+        child: this,
+      ),
+    );
+  }
+}
