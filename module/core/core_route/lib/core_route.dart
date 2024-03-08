5c00956b87e83466891ec4cadc4de24e1db87c31
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2024-03-08 08:56:54 +0700

    remove dep late object & base route args

--- module/core/core_route/lib/core_route.dart
+++ module/core/core_route/lib/core_route.dart
@@ -2,7 +2,5 @@ library core_route;
 
 export 'package:go_router/go_router.dart';
 
-export 'src/base_route_args.dart';
 export 'src/base_route_builder.dart';
-export 'src/navigation_result_mixin.dart';
 export 'src/route_observer.dart';
