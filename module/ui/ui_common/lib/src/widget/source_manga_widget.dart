bd1ac3b0363f26e6d5d5e01404756aa2c22cd9f4
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2024-03-09 17:24:55 +0700

    add shimmer manga source

--- module/ui/ui_common/lib/src/widget/source_manga_widget.dart
+++ module/ui/ui_common/lib/src/widget/source_manga_widget.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+import 'shimmer_loading_widget.dart';
+
 class SourceMangaWidget extends StatelessWidget {
   const SourceMangaWidget({
     super.key,
@@ -34,26 +36,48 @@ class SourceMangaWidget extends StatelessWidget {
     return ListTile(
       leading: SizedBox(
         height: double.infinity,
-        child: CachedNetworkImage(
-          imageUrl: iconUrl,
-          width: 16,
-          height: 16,
-          errorWidget: (context, url, error) => const Icon(Icons.error),
-          progressIndicatorBuilder: (context, url, downloadProgress) {
-            return Center(
-              child: SizedBox(
-                width: 16,
-                height: 16,
-                child: CircularProgressIndicator(
-                  value: downloadProgress.progress,
+        child: ShimmerLoading.circular(
+          isLoading: isLoading,
+          size: 16,
+          child: CachedNetworkImage(
+            imageUrl: iconUrl,
+            width: 16,
+            height: 16,
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Center(
+                child: SizedBox(
+                  width: 16,
+                  height: 16,
+                  child: CircularProgressIndicator(
+                    value: downloadProgress.progress,
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
-      title: Text(name),
-      subtitle: Text(url),
+      title: Align(
+        alignment: Alignment.centerLeft,
+        child: ShimmerLoading.multiline(
+          isLoading: isLoading,
+          width: 100,
+          height: 12,
+          lines: 1,
+          child: Text(name),
+        ),
+      ),
+      subtitle: Align(
+        alignment: Alignment.centerLeft,
+        child: ShimmerLoading.multiline(
+          isLoading: isLoading,
+          width: 200,
+          height: 10,
+          lines: 1,
+          child: Text(url),
+        ),
+      ),
       onTap: onTap,
       minLeadingWidth: 16,
     );
