93e96c1ad5fa6096289d6f448a60115cf11d3c9c
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2024-10-11 15:16:26 +0700

    fix failed import

--- module/library/background_worker/lib/src/background_worker.dart
+++ module/library/background_worker/lib/src/background_worker.dart
@@ -1,6 +1,6 @@
 import 'worker.dart'
-    if (dart.library.io) 'worker_io.dart'
-    if (dart.library.html) 'worker_web.dart';
+    if (dart.library.io) 'background_worker_io.dart'
+    if (dart.library.html) 'background_worker_web.dart';
 
 abstract class BackgroundWorker {
   factory BackgroundWorker() => getWorker();
