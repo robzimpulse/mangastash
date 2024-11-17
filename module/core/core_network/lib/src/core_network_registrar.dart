2759a4c327af4a342b3a33860c76dd5fdb1a46d0
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2024-11-17 14:08:55 +0700

    refactor dio manager

--- module/core/core_network/lib/src/core_network_registrar.dart
+++ module/core/core_network/lib/src/core_network_registrar.dart
@@ -1,11 +1,9 @@
 import 'dart:developer';
 
 import 'package:alice_lightweight/alice.dart';
-import 'package:dio/dio.dart';
-import 'package:dio_smart_retry/dio_smart_retry.dart';
 import 'package:service_locator/service_locator.dart';
 
-import 'interceptor/dio_throttler_interceptor.dart';
+import 'manager/dio_manager.dart';
 import 'manager/system_proxy_manager.dart';
 import 'manager/url_launcher_manager.dart';
 import 'use_case/get_proxy_use_case.dart';
@@ -23,42 +21,7 @@ class CoreNetworkRegistrar extends Registrar {
     locator.registerSingleton(UrlLauncherManager());
     locator.alias<LaunchUrlUseCase, UrlLauncherManager>();
 
-    locator.registerFactory(
-      () {
-        const userAgent = 'Mozilla/5.0 '
-            '(Macintosh; Intel Mac OS X 10_15_7) '
-            'AppleWebKit/537.36 (KHTML, like Gecko) '
-            'Chrome/127.0.0.0 '
-            'Safari/537.36';
-        final dio = Dio(BaseOptions(headers: {'User-Agent': userAgent}))
-          ..interceptors.addAll(
-            [
-              locator<Alice>().getDioInterceptor(),
-              DioThrottlerInterceptor(
-                const Duration(seconds: 1),
-                onThrottled: (req, scheduled) => log(
-                  'Delay request for ${req.uri} until $scheduled',
-                  name: runtimeType.toString(),
-                  time: DateTime.now(),
-                ),
-              ),
-            ],
-          );
-
-        return dio
-          ..interceptors.add(
-            RetryInterceptor(
-              dio: dio,
-              retryableExtraStatuses: {status400BadRequest},
-              logPrint: (msg) => log(
-                msg,
-                name: runtimeType.toString(),
-                time: DateTime.now(),
-              ),
-            ),
-          );
-      },
-    );
+    locator.registerSingleton(DioManager.create(alice: locator()));
     log('finish register', name: runtimeType.toString(), time: DateTime.now());
   }
 }
---
+++ module/core/core_network/lib/src/manager/dio_manager.dart
@@ -0,0 +1,47 @@
+import 'dart:developer';
+
+import 'package:alice_lightweight/alice.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_smart_retry/dio_smart_retry.dart';
+
+import '../interceptor/dio_throttler_interceptor.dart';
+
+class DioManager {
+  static Dio create({
+    required Alice alice,
+  }) {
+    const userAgent = 'Mozilla/5.0 '
+        '(Macintosh; Intel Mac OS X 10_15_7) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/127.0.0.0 '
+        'Safari/537.36';
+
+    final dio = Dio(BaseOptions(headers: {'User-Agent': userAgent}))
+      ..interceptors.addAll(
+        [
+          alice.getDioInterceptor(),
+          DioThrottlerInterceptor(
+            const Duration(seconds: 1),
+            onThrottled: (req, scheduled) => log(
+              'Delay request for ${req.uri} until $scheduled',
+              name: 'DioManager',
+              time: DateTime.now(),
+            ),
+          ),
+        ],
+      );
+
+    return dio
+      ..interceptors.add(
+        RetryInterceptor(
+          dio: dio,
+          retryableExtraStatuses: {status400BadRequest},
+          logPrint: (msg) => log(
+            msg,
+            name: 'DioManager',
+            time: DateTime.now(),
+          ),
+        ),
+      );
+  }
+}
