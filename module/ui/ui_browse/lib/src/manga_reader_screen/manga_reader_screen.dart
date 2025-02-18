bc49b392728395257411944bf557e2383f7d300e
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-02-18 13:15:58 +0700

    add annotated region

--- module/ui/ui_browse/lib/src/manga_reader_screen/manga_reader_screen.dart
+++ module/ui/ui_browse/lib/src/manga_reader_screen/manga_reader_screen.dart
@@ -1,6 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:core_storage/core_storage.dart';
 import 'package:entity_manga/entity_manga.dart';
-import 'package:flutter/services.dart';
 import 'package:safe_bloc/safe_bloc.dart';
 import 'package:service_locator/service_locator.dart';
 import 'package:ui_common/ui_common.dart';
@@ -9,7 +9,7 @@ import 'package:visibility_detector/visibility_detector.dart';
 import 'manga_reader_screen_cubit.dart';
 import 'manga_reader_screen_state.dart';
 
-class MangaReaderScreen extends StatefulWidget {
+class MangaReaderScreen extends StatelessWidget {
   const MangaReaderScreen({
     super.key,
     this.cacheManager,
@@ -46,11 +46,6 @@ class MangaReaderScreen extends StatefulWidget {
     );
   }
 
-  @override
-  State<MangaReaderScreen> createState() => _MangaReaderScreenState();
-}
-
-class _MangaReaderScreenState extends State<MangaReaderScreen> {
   MangaReaderScreenCubit _cubit(BuildContext context) => context.read();
 
   BlocBuilder _builder({
@@ -64,167 +59,194 @@ class _MangaReaderScreenState extends State<MangaReaderScreen> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-  }
-
-  @override
-  void dispose() {
-    SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.manual,
-      overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom],
-    );
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return ScaffoldScreen(
-      bottomSafeArea: false,
-      body: _content(context),
+      body: SafeArea(
+        child: _builder(
+          buildWhen: (prev, curr) => prev.isLoading != curr.isLoading,
+          builder: (context, state) => state.isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [_content(), _indicator()],
+                ),
+        ),
+      ),
     );
   }
 
-  Widget _content(BuildContext context) {
+  Widget _indicator() {
     return _builder(
-      buildWhen: (prev, curr) {
-        return prev.isLoading != curr.isLoading ||
-            prev.chapter?.images != curr.chapter?.images;
-      },
-      builder: (context, state) {
-        if (state.isLoading) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+      buildWhen: (prev, curr) => [
+        prev.progress != curr.progress,
+        prev.chapter?.images != curr.chapter?.images,
+      ].any((e) => e),
+      builder: (context, state) => Positioned(
+        bottom: double.minPositive,
+        child: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.5),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(4),
+              ),
+            ),
+            child: Text(
+              'Page ${state.progress} of ${state.chapter?.images?.length}',
+              style: const TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-        final images = state.chapter?.images ?? [];
+  Widget _content() {
+    return _builder(
+      buildWhen: (prev, curr) => [
+        prev.chapter?.images != curr.chapter?.images,
+      ].any((e) => e),
+      builder: (context, state) => Column(
+        children: [
+          Row(children: [Expanded(child: _prevButton())]),
+          Expanded(
+            child: CustomScrollView(
+              slivers: _images(
+                context: context,
+                images: state.chapter?.images ?? [],
+              ),
+            ),
+          ),
+          Row(children: [Expanded(child: _nextButton())]),
+        ],
+      ),
+    );
+  }
 
-        return Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            CustomScrollView(
-              slivers: [
-                SliverToBoxAdapter(
-                  child: _builder(
-                    buildWhen: (prev, curr) => [
-                      prev.previousChapterId != curr.previousChapterId,
-                    ].any((e) => e),
-                    builder: (context, state) => state.previousChapterId != null
-                        ? ElevatedButton(
-                            style: ButtonStyle(
-                              shape: WidgetStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                            ),
-                            onPressed: () => widget.onTapShortcut?.call(
-                              state.previousChapterId,
-                            ),
-                            child: const Text('Previous Chapter'),
-                          )
-                        : const SizedBox.shrink(),
-                  ),
+  Widget _prevButton() {
+    return _builder(
+      buildWhen: (prev, curr) => [
+        prev.previousChapterId != curr.previousChapterId,
+      ].any((e) => e),
+      builder: (context, state) => state.previousChapterId != null
+          ? ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
                 ),
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    childCount: images.length,
-                    (context, index) => VisibilityDetector(
-                      onVisibilityChanged: (info) {
-                        if (!context.mounted) return;
-                        _cubit(context).onVisibility(
-                          key: images[index],
-                          visibleFraction: info.visibleFraction,
-                        );
-                      },
-                      key: ValueKey<int>(index),
-                      child: CachedNetworkImage(
-                        cacheManager: widget.cacheManager,
-                        imageUrl: images[index],
-                        errorWidget: (context, url, error) => ConstrainedBox(
-                          constraints: const BoxConstraints(maxHeight: 300),
-                          child: Center(
-                            child: Row(
-                              children: [
-                                const Icon(Icons.error),
-                                const SizedBox(width: 8),
-                                Expanded(child: Text(error.toString())),
-                              ],
-                            ),
-                          ),
-                        ),
-                        progressIndicatorBuilder: (context, url, progress) {
-                          return ConstrainedBox(
-                            constraints: const BoxConstraints(maxHeight: 300),
-                            child: Center(
-                              child: SizedBox(
-                                width: 16,
-                                height: 16,
-                                child: CircularProgressIndicator(
-                                  value: progress.progress,
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
+              ),
+              onPressed: () => onTapShortcut?.call(
+                state.previousChapterId,
+              ),
+              child: const Text('Previous Chapter'),
+            )
+          : const SizedBox.shrink(),
+    );
+  }
+
+  Widget _nextButton() {
+    return _builder(
+      buildWhen: (prev, curr) => [
+        prev.nextChapterId != curr.nextChapterId,
+      ].any((e) => e),
+      builder: (context, state) => state.nextChapterId != null
+          ? ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onPressed: () => onTapShortcut?.call(
+                state.nextChapterId,
+              ),
+              child: const Text('Next Chapter'),
+            )
+          : const SizedBox.shrink(),
+    );
+  }
+
+  List<Widget> _images({
+    required BuildContext context,
+    required List<String> images,
+  }) {
+    if (images.isEmpty) {
+      return [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text('Images Empty'),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                ),
-                SliverToBoxAdapter(
-                  child: _builder(
-                    buildWhen: (prev, curr) => [
-                      prev.nextChapterId != curr.nextChapterId,
-                    ].any((e) => e),
-                    builder: (context, state) => state.nextChapterId != null
-                        ? ElevatedButton(
-                            style: ButtonStyle(
-                              shape: WidgetStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                            ),
-                            onPressed: () => widget.onTapShortcut?.call(
-                              state.nextChapterId,
-                            ),
-                            child: const Text('Next Chapter'),
-                          )
-                        : const SizedBox.shrink(),
+                  onPressed: () => context.showSnackBar(
+                    message: '🚧🚧🚧 Under Construction 🚧🚧🚧',
                   ),
+                  child: const Text('Open Debug Browser'),
                 ),
               ],
             ),
-            _builder(
-              buildWhen: (prev, curr) => prev.progress != curr.progress,
-              builder: (context, state) => Positioned(
-                bottom: double.minPositive,
-                child: SafeArea(
-                  child: Container(
-                    padding: const EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.5),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(4),
-                      ),
-                    ),
-                    child: Text(
-                      'Page ${state.progress} of ${images.length}',
-                      style: const TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
+          ),
+        ),
+      ];
+    }
+
+    return List.from(
+      images.mapIndexed(
+        (index, image) => SliverToBoxAdapter(
+          child: VisibilityDetector(
+            key: ValueKey<int>(index),
+            onVisibilityChanged: (info) {
+              if (!context.mounted) return;
+              _cubit(context).onVisibility(
+                key: '$index',
+                visibleFraction: info.visibleFraction,
+              );
+            },
+            child: CachedNetworkImage(
+              cacheManager: cacheManager,
+              imageUrl: image,
+              errorWidget: (context, url, error) => ConstrainedBox(
+                constraints: const BoxConstraints(maxHeight: 300),
+                child: Center(
+                  child: Row(
+                    children: [
+                      const Icon(Icons.error),
+                      const SizedBox(width: 8),
+                      Expanded(child: Text(error.toString())),
+                    ],
                   ),
                 ),
               ),
+              progressIndicatorBuilder: (context, url, progress) {
+                return ConstrainedBox(
+                  constraints: const BoxConstraints(maxHeight: 300),
+                  child: Center(
+                    child: SizedBox(
+                      width: 16,
+                      height: 16,
+                      child: CircularProgressIndicator(
+                        value: progress.progress,
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          ],
-        );
-      },
+          ),
+        ),
+      ),
     );
   }
 }
--- module/ui/ui_common/lib/src/scaffold_screen.dart
+++ module/ui/ui_common/lib/src/scaffold_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'widget/shimmer_area_widget.dart';
 
@@ -30,18 +31,24 @@ class ScaffoldScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: canPop,
-      onPopInvoked: onPopInvoked,
-      child: ShimmerAreaWidget(
-        child: Scaffold(
-          backgroundColor: backgroundColor,
-          appBar: appBar,
-          bottomNavigationBar: bottomNavigationBar,
-          body: SafeArea(
-            top: false,
-            bottom: bottomSafeArea,
-            child: body,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor:
+            backgroundColor ?? Theme.of(context).appBarTheme.backgroundColor,
+      ),
+      child: PopScope(
+        canPop: canPop,
+        onPopInvoked: onPopInvoked,
+        child: ShimmerAreaWidget(
+          child: Scaffold(
+            backgroundColor: backgroundColor,
+            appBar: appBar,
+            bottomNavigationBar: bottomNavigationBar,
+            body: SafeArea(
+              top: false,
+              bottom: bottomSafeArea,
+              child: body,
+            ),
           ),
         ),
       ),
