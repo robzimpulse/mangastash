79f8bfc5cf95ab15054b134cac20613a46794508
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-05-21 10:50:48 +0700

    fix image crash

--- module/ui/ui_common/lib/src/widget/cached_network_image_widget.dart
+++ module/ui/ui_common/lib/src/widget/cached_network_image_widget.dart
@@ -36,22 +36,56 @@ class CachedNetworkImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cache = cacheManager;
     return SizedBox(
       width: width,
       height: height,
-      child: Image.network(
-        imageUrl,
-        fit: fit,
-        headers: headers,
-        loadingBuilder: (context, child, event) {
-          final progress = event?.progress;
-          if (progress == null) return child;
-          return progressBuilder?.call(context, progress) ?? child;
-        },
-        errorBuilder: errorBuilder,
-        width: width,
-        height: height,
-      ),
+      child: cache == null
+          ? Image.network(
+              imageUrl,
+              fit: fit,
+              headers: headers,
+              loadingBuilder: (context, child, event) {
+                final progress = event?.progress;
+                if (progress == null) return child;
+                return progressBuilder?.call(context, progress) ?? child;
+              },
+              errorBuilder: errorBuilder,
+              width: width,
+              height: height,
+            )
+          : StreamBuilder(
+              stream: cache.getFileStream(
+                imageUrl,
+                headers: headers,
+                withProgress: true,
+              ),
+              builder: (context, snapshot) {
+                final data = snapshot.data;
+                const fallback = SizedBox.shrink();
+
+                if (data == null) {
+                  return progressBuilder?.call(context, 0.0) ?? fallback;
+                }
+
+                if (data is FileInfo) {
+                  return Image.file(
+                    data.file,
+                    fit: fit,
+                    width: width,
+                    height: height,
+                    errorBuilder: errorBuilder,
+                  );
+                }
+
+                if (data is DownloadProgress) {
+                  final progress = data.progress ?? 0;
+                  return progressBuilder?.call(context, progress) ?? fallback;
+                }
+
+                return fallback;
+              },
+            ),
     );
   }
 }
