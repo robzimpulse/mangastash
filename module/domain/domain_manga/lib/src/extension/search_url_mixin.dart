aa3f4b87064c61246e3ddf38efc2664d8c0ce292
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-06-12 14:48:24 +0700

    add todo

--- module/domain/domain_manga/lib/src/extension/search_url_mixin.dart
+++ module/domain/domain_manga/lib/src/extension/search_url_mixin.dart
@@ -1,4 +1,5 @@
-import '../../domain_manga.dart';
+import 'package:core_environment/core_environment.dart';
+import 'package:manga_dex_api/manga_dex_api.dart';
 
 extension SearchUrlMixin on SearchMangaParameter {
   String get mangaclash {
@@ -18,6 +19,44 @@ extension SearchUrlMixin on SearchMangaParameter {
       }.entries.map((e) => '${e.key}=${e.value}').join('&'),
     ].join('?');
   }
+
+  // TODO: fix this
+  // String get mangaclash {
+  //   return [
+  //     [
+  //       'https://toonclash.com',
+  //       'page',
+  //       '$page',
+  //     ].join('/'),
+  //     [
+  //       const MapEntry('post_type', 'wp-manga'),
+  //       title?.let((value) => MapEntry('s', title)),
+  //       if (orders?.containsKey(SearchOrders.rating) == true)
+  //         const MapEntry('m_orderby', 'rating'),
+  //       if (orders?.containsKey(SearchOrders.updatedAt) == true)
+  //         const MapEntry('m_orderby', 'latest'),
+  //       for (final status in status ?? <MangaStatus>[])
+  //         MapEntry(
+  //           'status[]',
+  //           switch (status) {
+  //             MangaStatus.ongoing => 'on-going',
+  //             MangaStatus.completed => 'end',
+  //             MangaStatus.hiatus => 'on-hold',
+  //             MangaStatus.cancelled => 'canceled',
+  //           },
+  //         ),
+  //       for (final tag in includedTags ?? <String>[])
+  //         MapEntry('genre[]', tag.toLowerCase().replaceAll(' ', '-')),
+  //       if (includedTags?.isNotEmpty == true)
+  //         includedTagsMode?.let(
+  //               (value) => switch (value) {
+  //             TagsMode.or => const MapEntry('op', ''),
+  //             TagsMode.and => const MapEntry('op', '1'),
+  //           },
+  //         ),
+  //     ].nonNulls.map((e) => '${e.key}=${e.value}').join('&'),
+  //   ].join('?');
+  // }
 
   String get asurascan {
     return [
