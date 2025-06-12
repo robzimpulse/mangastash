e56075b0ff84b7ed0c104f54b8bccb95a0369a5f
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-06-12 16:07:25 +0700

    fix param

--- module/domain/domain_manga/lib/src/extension/search_url_mixin.dart
+++ module/domain/domain_manga/lib/src/extension/search_url_mixin.dart
@@ -9,54 +9,35 @@ extension SearchUrlMixin on SearchMangaParameter {
         'page',
         '$page',
       ].join('/'),
-      {
-        's': title ?? '',
-        'post_type': 'wp-manga',
+      [
+        const MapEntry('post_type', 'wp-manga'),
+        MapEntry('s', title ?? ''),
         if (orders?.containsKey(SearchOrders.rating) == true)
-          'm_orderby': 'rating',
+          const MapEntry('m_orderby', 'rating'),
         if (orders?.containsKey(SearchOrders.updatedAt) == true)
-          'm_orderby': 'latest',
-      }.entries.map((e) => '${e.key}=${e.value}').join('&'),
+          const MapEntry('m_orderby', 'latest'),
+        for (final status in status ?? <MangaStatus>[])
+          MapEntry(
+            'status[]',
+            switch (status) {
+              MangaStatus.ongoing => 'on-going',
+              MangaStatus.completed => 'end',
+              MangaStatus.hiatus => 'on-hold',
+              MangaStatus.cancelled => 'canceled',
+            },
+          ),
+        for (final tag in includedTags ?? <String>[])
+          MapEntry('genre[]', tag.toLowerCase().replaceAll(' ', '-')),
+        if (includedTags?.isNotEmpty == true)
+          includedTagsMode?.let(
+                (value) => switch (value) {
+              TagsMode.or => const MapEntry('op', ''),
+              TagsMode.and => const MapEntry('op', '1'),
+            },
+          ),
+      ].nonNulls.map((e) => '${e.key}=${e.value}').join('&'),
     ].join('?');
   }
-
-  // TODO: fix this
-  // String get mangaclash {
-  //   return [
-  //     [
-  //       'https://toonclash.com',
-  //       'page',
-  //       '$page',
-  //     ].join('/'),
-  //     [
-  //       const MapEntry('post_type', 'wp-manga'),
-  //       title?.let((value) => MapEntry('s', title)),
-  //       if (orders?.containsKey(SearchOrders.rating) == true)
-  //         const MapEntry('m_orderby', 'rating'),
-  //       if (orders?.containsKey(SearchOrders.updatedAt) == true)
-  //         const MapEntry('m_orderby', 'latest'),
-  //       for (final status in status ?? <MangaStatus>[])
-  //         MapEntry(
-  //           'status[]',
-  //           switch (status) {
-  //             MangaStatus.ongoing => 'on-going',
-  //             MangaStatus.completed => 'end',
-  //             MangaStatus.hiatus => 'on-hold',
-  //             MangaStatus.cancelled => 'canceled',
-  //           },
-  //         ),
-  //       for (final tag in includedTags ?? <String>[])
-  //         MapEntry('genre[]', tag.toLowerCase().replaceAll(' ', '-')),
-  //       if (includedTags?.isNotEmpty == true)
-  //         includedTagsMode?.let(
-  //               (value) => switch (value) {
-  //             TagsMode.or => const MapEntry('op', ''),
-  //             TagsMode.and => const MapEntry('op', '1'),
-  //           },
-  //         ),
-  //     ].nonNulls.map((e) => '${e.key}=${e.value}').join('&'),
-  //   ].join('?');
-  // }
 
   String get asurascan {
     return [
