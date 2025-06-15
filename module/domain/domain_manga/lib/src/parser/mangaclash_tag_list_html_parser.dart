d15c51ad1b7ef13a3d11637842e6a66956bb76b0
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-06-15 13:04:15 +0700

    add tags scrapper mangaclash

--- module/domain/domain_manga/lib/src/parser/mangaclash_tag_list_html_parser.dart
+++ module/domain/domain_manga/lib/src/parser/mangaclash_tag_list_html_parser.dart
@@ -1,15 +1,21 @@
+import 'package:core_environment/core_environment.dart';
 import 'package:entity_manga/src/tag.dart';
 
 import 'base/tag_list_html_parser.dart';
 
 class MangaClashTagListHtmlParser extends TagListHtmlParser {
-
   @override
   List<Tag> get tags {
-    // TODO: implement tags
-    return [];
+    final region = root.querySelector('div.form-group.checkbox-group.row');
+
+    return [
+      for (final child in [...?region?.children])
+        Tag(
+          id: child.querySelector('input')?.attributes['value'],
+          name: toBeginningOfSentenceCase(child.text.trim()),
+        ),
+    ];
   }
 
   MangaClashTagListHtmlParser({required super.root});
-
 }
