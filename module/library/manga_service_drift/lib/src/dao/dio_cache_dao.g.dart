f2ef2371e35e13aac07aecc520e0023c38dffaa3
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-06-30 11:13:28 +0700

    forgot commit

---
+++ module/library/manga_service_drift/lib/src/dao/dio_cache_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dio_cache_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$DioCacheDaoMixin on DatabaseAccessor<AppDatabase> {
+  $DioCacheTablesTable get dioCacheTables => attachedDatabase.dioCacheTables;
+}
