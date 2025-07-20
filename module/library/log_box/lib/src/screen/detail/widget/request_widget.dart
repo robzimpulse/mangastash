0615c20dd236a20caf85a3afd8cbc994cc3adebb
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-07-20 14:31:54 +0700

    add response and request widget

--- module/library/log_box/lib/src/screen/detail/widget/request_widget.dart
+++ module/library/log_box/lib/src/screen/detail/widget/request_widget.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
+import '../../../common/extension.dart';
 import '../../../model/entry.dart';
+import '../../../model/network_entry.dart';
+import 'item_column.dart';
 
 class RequestWidget extends StatelessWidget {
   const RequestWidget({super.key, required this.data});
@@ -9,8 +13,44 @@ class RequestWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+    final data = this.data;
 
+    return CustomScrollView(
+      slivers: [
+        if (data is NetworkEntry) ...[
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            sliver: MultiSliver(
+              children: [
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'Timestamp',
+                    value: data.request?.time.toIso8601String(),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'Headers',
+                    value: data.request?.headers,
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'Query',
+                    value: data.request?.queryParameters.json,
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'Body',
+                    value: data.request?.body,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ],
+    );
+  }
 }
--- module/library/log_box/lib/src/screen/detail/widget/response_widget.dart
+++ module/library/log_box/lib/src/screen/detail/widget/response_widget.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
+import '../../../common/extension.dart';
 import '../../../model/entry.dart';
+import '../../../model/network_entry.dart';
+import 'item_column.dart';
 
 class ResponseWidget extends StatelessWidget {
   const ResponseWidget({super.key, required this.data});
@@ -9,8 +13,41 @@ class ResponseWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+    final data = this.data;
 
+    return CustomScrollView(
+      slivers: [
+        if (data is NetworkEntry) ...[
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            sliver: MultiSliver(
+              children: [
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'Timestamp',
+                    value: data.response?.time.toIso8601String(),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'Size',
+                    value: data.response?.size.toString(),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'Headers',
+                    value: data.response?.headers?.json,
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(name: 'Body', value: data.response?.body),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ],
+    );
+  }
 }
