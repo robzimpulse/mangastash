928209426d8ed0d79a001a3d71a6a515fabe8ba5
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-07-20 10:23:35 +0700

    add sliver tools

--- module/library/log_box/lib/src/screen/detail/detail_screen.dart
+++ module/library/log_box/lib/src/screen/detail/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 import '../../common/extension.dart';
 import '../../model/entry.dart';
@@ -47,7 +48,10 @@ class DetailScreen extends StatelessWidget {
         unselectedLabelColor: Colors.white,
         tabs: [
           Tab(text: 'Overview', icon: Icon(Icons.info, color: Colors.white)),
-          Tab(text: 'Detail', icon: Icon(Icons.data_array, color: Colors.white)),
+          Tab(
+            text: 'Detail',
+            icon: Icon(Icons.data_array, color: Colors.white),
+          ),
           Tab(text: 'Error', icon: Icon(Icons.error, color: Colors.white)),
         ],
       ),
@@ -56,50 +60,71 @@ class DetailScreen extends StatelessWidget {
 
   Widget _overviewWidget() {
     final data = this.data;
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: Column(
-          children: [
-            if (data is LogEntry) ...[
-              ItemColumn(name: 'Name', value: data.name),
-              ItemColumn(name: 'Message', value: data.message),
-            ] else if (data is NetworkEntry) ...[
-              ItemColumn(name: 'Method', value: data.method),
-              ItemColumn(name: 'Url', value: data.uri),
-            ] else if (data is WebviewEntry) ...[
-              ItemColumn(name: 'url', value: data.uri.toString()),
-              ItemColumn(name: 'scripts', value: jsonEncode(data.scripts)),
+
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          sliver: MultiSliver(
+            children: [
+              SliverToBoxAdapter(
+                child: ItemColumn(
+                  name: 'Timestamp',
+                  value: data.timestamp.toIso8601String(),
+                ),
+              ),
+              if (data is LogEntry) ...[
+                SliverToBoxAdapter(
+                  child: ItemColumn(name: 'Name', value: data.name),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(name: 'Message', value: data.message),
+                ),
+              ] else if (data is NetworkEntry) ...[
+                SliverToBoxAdapter(
+                  child: ItemColumn(name: 'Method', value: data.method),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(name: 'Url', value: data.uri),
+                ),
+              ] else if (data is WebviewEntry) ...[
+                SliverToBoxAdapter(
+                  child: ItemColumn(name: 'url', value: data.uri.toString()),
+                ),
+                SliverToBoxAdapter(
+                  child: ItemColumn(
+                    name: 'scripts',
+                    value: jsonEncode(data.scripts),
+                  ),
+                ),
+              ],
             ],
-            ItemColumn(
-              name: 'Timestamp',
-              value: data.timestamp.toIso8601String(),
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 
   Widget _extraWidget() {
     final data = this.data;
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: Column(
-          children: [
-            if (data is LogEntry) ...[
-              ItemColumn(name: 'Extra', value: data.extra.json),
-            ] else if (data is NetworkEntry) ...[
-              // TODO: add item column
-            ] else if (data is WebviewEntry) ...[
-              ItemColumn(name: 'Html', value: data.html),
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          sliver: MultiSliver(
+            children: [
+              if (data is LogEntry) ...[
+                SliverToBoxAdapter(child: ItemColumn(name: 'Extra', value: data.extra.json)),
+              ] else if (data is NetworkEntry) ...[
+                // SliverToBoxAdapter(child: ItemColumn(name: 'Request', value: data.request?.json),),
+                // SliverToBoxAdapter(child: ItemColumn(name: 'Response', value: data.request?.json),),
+              ] else if (data is WebviewEntry) ...[
+                SliverToBoxAdapter(child: ItemColumn(name: 'Html', value: data.html)),
+              ],
             ],
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 
