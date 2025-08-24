34312952dcb71ba63d84e45a02afccebd0aecebd
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-08-24 22:03:27 +0700

    bump lints and add super paging

---
+++ module/ui/ui_browse/lib/widgets/paginated_manga_grid.dart
@@ -0,0 +1,12 @@
+import 'package:feature_common/feature_common.dart';
+
+class PaginatedMangaGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+
+}
