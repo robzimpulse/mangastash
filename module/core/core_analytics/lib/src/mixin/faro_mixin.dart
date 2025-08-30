e8187606b6f8faeb95abfedeeb1248494bb7204f
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-08-30 08:08:08 +0700

    fix run app

--- module/core/core_analytics/lib/src/mixin/faro_mixin.dart
+++ module/core/core_analytics/lib/src/mixin/faro_mixin.dart
@@ -1,8 +1,6 @@
-import 'dart:developer';
-
 import 'package:faro/faro.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart' as widget;
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:universal_io/io.dart';
 
@@ -10,10 +8,9 @@ mixin FaroMixin {
   /// faro only support ios and android only
   Faro? get faro => Platform.isAndroid || Platform.isIOS ? Faro() : null;
 
-  static void runApp(Widget app) async {
+  static void runApp(widget.Widget app) async {
     if (!(Platform.isAndroid || Platform.isIOS)) {
-      log('Running Without Faro: Platform not android or ios', name: 'Mangastash');
-      runApp(app);
+      widget.runApp(app);
       return;
     }
 
@@ -21,16 +18,13 @@ mixin FaroMixin {
     const faroCollectorUrl = String.fromEnvironment('FARO_COLLECTOR_URL');
 
     if (faroApiKey.isEmpty || faroCollectorUrl.isEmpty) {
-      log('Running Without Faro: api key and collector url empty', name: 'Mangastash');
-      runApp(app);
+      widget.runApp(app);
       return;
     }
 
     HttpOverrides.global = FaroHttpOverrides(HttpOverrides.current);
 
     final info = await PackageInfo.fromPlatform();
-
-    log('Start Running With Faro', name: 'Mangastash');
 
     Faro().runApp(
       optionsConfiguration: FaroConfig(
@@ -45,9 +39,8 @@ mixin FaroMixin {
         namespace: 'flutter',
       ),
       appRunner: () {
-        log('Running With Faro', name: 'Mangastash');
-        return runApp(
-          DefaultAssetBundle(
+        return widget.runApp(
+          widget.DefaultAssetBundle(
             bundle: FaroAssetBundle(),
             child: FaroUserInteractionWidget(child: app),
           ),
