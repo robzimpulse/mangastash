050350d7f533e6bbf8d4a4accb567bc264bd4276
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-09-02 14:06:43 +0700

    remove faro

--- module/core/core_analytics/lib/core_analytics.dart
+++ module/core/core_analytics/lib/core_analytics.dart
@@ -1,6 +1,5 @@
 library core_analytics;
 
-export 'package:faro/faro.dart';
 export 'package:log_box/log_box.dart';
 export 'package:log_box_dio_logger/log_box_dio_logger.dart';
 export 'package:log_box_in_app_webview_logger/log_box_in_app_webview_logger.dart';
