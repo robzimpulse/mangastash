f355fe341fd423d2ea257294e33d8b4fe41df98c
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-10-30 10:46:58 +0700

    remove wrong key

--- module/domain/domain_manga/lib/src/use_case/manga/search_manga_use_case.dart
+++ module/domain/domain_manga/lib/src/use_case/manga/search_manga_use_case.dart
@@ -118,7 +118,6 @@ class SearchMangaUseCase with SyncMangasMixin {
   Future<void> clear({required SourceSearchMangaParameter parameter}) async {
     final data = await _searchMangaCacheManager.keys;
     final List<Future<void>> promises = [
-      _searchMangaCacheManager.removeFile(parameter.source.icon),
       _htmlCacheManager.removeFile(parameter.source.icon),
     ];
     for (final value in data) {
