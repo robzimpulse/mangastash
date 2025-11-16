6081fb0336a2b13aa40ff6ca500dc488c8465dc6
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-11-16 19:32:51 +0700

    add initial manga dao test

--- module/library/manga_service_drift/test/manga_dao_test.dart
+++ module/library/manga_service_drift/test/manga_dao_test.dart
@@ -1,81 +1,68 @@
-// import 'package:drift/drift.dart';
-// import 'package:drift/native.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:manga_service_drift/manga_service_drift.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:manga_service_drift/manga_service_drift.dart';
 
 void main() {
-  // late AppDatabase db;
-  // late MangaDao mangaDao;
-  //
-  // final mangas = List.generate(
-  //   10,
-  //   (index) => MangaTablesCompanion(
-  //     id: Value('manga_$index'),
-  //     title: Value('title_$index'),
-  //     coverUrl: Value('cover_url_$index'),
-  //     status: Value('status_$index'),
-  //     author: Value('value_$index'),
-  //     description: Value('description_$index'),
-  //     webUrl: Value('web_url_$index'),
-  //     source: Value('source_$index'),
-  //   ),
-  // );
-  //
-  // final tags = List.generate(
-  //   10,
-  //   (index) => TagTablesCompanion(
-  //     id: Value('tag_$index'),
-  //     name: Value('name_$index'),
-  //   ),
-  // );
-  //
-  // setUp(() {
-  //   db = AppDatabase(
-  //     executor: DatabaseConnection(
-  //       NativeDatabase.memory(),
-  //       closeStreamsSynchronously: true,
-  //     ),
-  //   );
-  //   mangaDao = MangaDao(db);
-  // });
-  //
-  // tearDown(() => db.close());
-  //
-  // group('Manga Dao Test', () {
-  //   tearDown(() => db.clear());
-  //
-  //   setUp(() async {
-  //     for (final tag in tags) {
-  //       await mangaDao.insertTag(tag);
-  //     }
-  //
-  //     for (final manga in mangas) {
-  //       await mangaDao.insertManga(manga);
-  //     }
-  //   });
-  //
-  //   group('Get Manga', () {
-  //     test('With Non Empty Tags', () async {
-  //       for (final manga in mangas) {
-  //         await mangaDao.unlinkAllTagFromManga(manga.id.value);
-  //         await mangaDao.linkTagToManga(
-  //           manga.id.value,
-  //           tags.map((e) => e.id.value),
-  //         );
-  //       }
-  //
-  //       final result = await mangaDao.getManga('manga_0');
-  //       expect(result != null, isTrue);
-  //       expect(result?.$1.id, 'manga_0');
-  //       expect(result?.$2.length, 10);
-  //     });
-  //
-  //     test('With Empty Tags', () async {
-  //       final result = await mangaDao.getManga('manga_0');
-  //       expect(result != null, isTrue);
-  //       expect(result?.$1.id, 'manga_0');
-  //       expect(result?.$2.length, 0);
-  //     });
-  //   });
-  // });
+  late AppDatabase db;
+  late MangaDao dao;
+
+  final mangas = List.generate(
+    10,
+    (mangaIdx) => (
+      MangaTablesCompanion(
+        id: Value('manga_$mangaIdx'),
+        title: Value('title_$mangaIdx'),
+        coverUrl: Value('cover_url_$mangaIdx'),
+        status: Value('status_$mangaIdx'),
+        author: Value('value_$mangaIdx'),
+        description: Value('description_$mangaIdx'),
+        webUrl: Value('web_url_$mangaIdx'),
+        source: Value('source_$mangaIdx'),
+      ),
+      List.generate(10, (index) => 'name_$index'),
+    ),
+  );
+
+  setUp(() {
+    db = AppDatabase(
+      executor: DatabaseConnection(
+        NativeDatabase.memory(),
+        closeStreamsSynchronously: true,
+      ),
+    );
+    dao = MangaDao(db);
+  });
+
+  tearDown(() => db.close());
+
+  group('Manga Dao Test', () {
+    setUp(() async {
+      await dao.adds(values: {for (final manga in mangas) manga.$1: manga.$2});
+    });
+    //
+    //   group('Get Manga', () {
+    //     test('With Non Empty Tags', () async {
+    //       for (final manga in mangas) {
+    //         await mangaDao.unlinkAllTagFromManga(manga.id.value);
+    //         await mangaDao.linkTagToManga(
+    //           manga.id.value,
+    //           tags.map((e) => e.id.value),
+    //         );
+    //       }
+    //
+    //       final result = await mangaDao.getManga('manga_0');
+    //       expect(result != null, isTrue);
+    //       expect(result?.$1.id, 'manga_0');
+    //       expect(result?.$2.length, 10);
+    //     });
+    //
+    //     test('With Empty Tags', () async {
+    //       final result = await mangaDao.getManga('manga_0');
+    //       expect(result != null, isTrue);
+    //       expect(result?.$1.id, 'manga_0');
+    //       expect(result?.$2.length, 0);
+    //     });
+    //   });
+  });
 }
