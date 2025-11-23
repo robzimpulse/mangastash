b3c6b5476ac1c93e3321c4ccaaca3202783a312d
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-11-23 17:12:47 +0700

    after recrawl, refresh image without cache

--- module/ui/ui_browse/lib/src/manga_reader_screen/manga_reader_screen_cubit.dart
+++ module/ui/ui_browse/lib/src/manga_reader_screen/manga_reader_screen_cubit.dart
@@ -43,12 +43,15 @@ class MangaReaderScreenCubit extends Cubit<MangaReaderScreenState>
     await super.close();
   }
 
-  Future<void> init() async {
+  Future<void> init({bool useCache = true}) async {
     emit(state.copyWith(error: () => null));
-    await Future.wait([_fetchChapter(), _fetchPreviousAndNextChapter()]);
+    await Future.wait([
+      _fetchChapter(useCache: useCache),
+      _fetchPreviousAndNextChapter(useCache: useCache),
+    ]);
   }
 
-  Future<void> _fetchPreviousAndNextChapter() async {
+  Future<void> _fetchPreviousAndNextChapter({bool useCache = true}) async {
     final mangaId = state.mangaId;
     final source = state.source;
 
@@ -60,6 +63,7 @@ class MangaReaderScreenCubit extends Cubit<MangaReaderScreenState>
       parameter: state.parameter.copyWith(
         orders: {ChapterOrders.chapter: OrderDirections.ascending},
       ),
+      useCache: useCache,
     );
 
     emit(state.copyWith(chapterIds: [...response.map((e) => e.id).nonNulls]));
@@ -77,7 +81,7 @@ class MangaReaderScreenCubit extends Cubit<MangaReaderScreenState>
     );
   }
 
-  Future<void> _fetchChapter() async {
+  Future<void> _fetchChapter({bool useCache = true}) async {
     final chapterId = state.chapterId;
     final mangaId = state.mangaId;
     final source = state.source;
@@ -97,6 +101,7 @@ class MangaReaderScreenCubit extends Cubit<MangaReaderScreenState>
       chapterId: chapterId,
       mangaId: mangaId,
       source: source,
+      useCache: useCache,
     );
 
     if (response is Success<Chapter>) {
@@ -113,6 +118,6 @@ class MangaReaderScreenCubit extends Cubit<MangaReaderScreenState>
   void recrawl({required BuildContext context, required String url}) async {
     await _recrawlUseCase.execute(context: context, url: url);
     await _clearChapterCache();
-    await init();
+    await init(useCache: false);
   }
 }
