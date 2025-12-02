5093c98c21d9ad8d071daaabbc52227b0508c7d3
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-12-02 09:48:30 +0700

    remove mock timezone

--- test/extension/patrol_tester_extension.dart
+++ test/extension/patrol_tester_extension.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 
 import 'package:core_analytics/core_analytics.dart';
 import 'package:core_environment/core_environment.dart';
@@ -6,8 +5,8 @@ import 'package:core_network/core_network.dart';
 import 'package:core_route/core_route.dart';
 import 'package:core_storage/core_storage.dart';
 import 'package:domain_manga/domain_manga.dart';
-import 'package:feature_common/feature_common.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:manga_service_drift/src/database/memory_executor.dart';
 import 'package:mangastash/screen/apps_screen.dart';
@@ -17,7 +16,7 @@ import 'package:mangastash/screen/wrapper_screen.dart';
 import 'package:patrol_finders/patrol_finders.dart';
 import 'package:service_locator/service_locator.dart';
 
-import '../mock/mock_get_timezone_use_case.dart';
+import '../fake/fake_method_channel.dart';
 import '../mock/mock_shared_preferences_async.dart';
 import '../mock/mock_storage_manager.dart';
 
@@ -34,6 +33,7 @@ void testScreen(
   double dpi = 2.625,
   double textScaleFactor = 1.1,
   String timezone = 'Asia/Jakarta',
+  List<String> availableTimezone = const ['Asia/Jakarta'],
 }) {
   ServiceLocatorInitiator.setServiceLocatorFactory(
     () => GetItServiceLocator()..setAllowReassignment(true),
@@ -47,10 +47,46 @@ void testScreen(
 
     TestWidgetsFlutterBinding.ensureInitialized();
     final locator = ServiceLocator.asNewInstance();
+
+    final binding = $.tester.binding.defaultBinaryMessenger;
+    final methodChannel = FakeMethodChannel(
+      onChannelsChanged: (channels) {
+        for (final channel in channels.entries) {
+          binding.setMockMethodCallHandler(channel.key, channel.value);
+        }
+      },
+      onChannelTriggered: (channel, method) {
+        return binding.handlePlatformMessage(
+          channel.name,
+          channel.codec.encodeMethodCall(method),
+          (_) {},
+        );
+      },
+    );
+
+    methodChannel.addChannel(
+      channel: MethodChannel('flutter_timezone'),
+      handler: (method) async {
+        if (method.method == 'getLocalTimezone') {
+          return timezone;
+        }
+
+        if (method.method == 'getAvailableTimezones') {
+          return availableTimezone;
+        }
+
+        throw Exception(
+          'Unhandled [${method.method}] with argument [${method.arguments}]',
+        );
+      },
+    );
+
     await $.pumpWidget(
       WrapperScreen(
         locatorBuilder: () {
           return Future(() async {
+            locator.registerSingleton(methodChannel);
+
             // TODO: register module registrar here
             await locator.registerRegistrar(CoreAnalyticsRegistrar());
             await locator.registerRegistrar(CoreStorageRegistrar());
@@ -59,9 +95,6 @@ void testScreen(
             await locator.registerRegistrar(CoreRouteRegistrar());
             await locator.registerRegistrar(DomainMangaRegistrar());
 
-            locator.registerSingleton<GetTimeZoneUseCase>(
-              MockGetTimezoneUseCase()..setLocal(timezone: timezone),
-            );
             locator.registerSingleton<SharedPreferencesAsync>(
               MockSharedPreferencesAsync(),
             );
