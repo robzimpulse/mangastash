ddf54505d83a20922c6b4251d900b1266d1f6eaa
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-12-04 14:12:32 +0700

    change using dialog

--- module/core/core_storage/lib/src/use_case/filesystem_picker_usecase.dart
+++ module/core/core_storage/lib/src/use_case/filesystem_picker_usecase.dart
@@ -19,7 +19,11 @@ class FilesystemPickerUsecase {
 
   Future<Result<Directory>> directory(BuildContext context) async {
     try {
-      final path = await picker(context, FilesystemType.folder);
+      final path = await picker(
+        context,
+        FilesystemType.folder,
+        contextActions: [FilesystemPickerNewFolderContextAction()],
+      );
       if (path == null) throw Exception('Path is null');
       return Success(Directory.fromUri(Uri.file(path)));
     } catch (e) {
@@ -37,15 +41,19 @@ class FilesystemPickerUsecase {
     }
   }
 
-  Future<String?> picker(BuildContext context, FilesystemType? type) {
-    return FilesystemPicker.open(
+  Future<String?> picker(
+    BuildContext context,
+    FilesystemType? type, {
+    List<FilesystemPickerContextAction> contextActions = const [],
+  }) {
+    return FilesystemPicker.openDialog(
       title: 'Select file or folder',
       context: context,
       rootDirectory: _getRootPathUseCase.rootPath,
       directory: _listenBackupPathUseCase.backupPathStream.valueOrNull,
       fsType: type,
       pickText: 'Select current folder',
-      contextActions: [FilesystemPickerNewFolderContextAction()],
+      contextActions: contextActions,
       requestPermission: () async {
         await [Permission.manageExternalStorage, Permission.storage].request();
 
