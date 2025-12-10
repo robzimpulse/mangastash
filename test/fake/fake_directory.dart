b1652f76b4c6f1205c3e7ebd31869dd85d5a7647
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-12-10 14:56:34 +0700

    fix failed unit test

--- test/fake/fake_directory.dart
+++ test/fake/fake_directory.dart
@@ -2,9 +2,11 @@ import 'package:mocktail/mocktail.dart';
 import 'package:universal_io/universal_io.dart';
 
 class FakeDirectory extends Mock implements Directory {
-
   FakeDirectory() {
     when(() => path).thenReturn('fake/directory');
     when(() => exists()).thenAnswer((_) async => true);
+    when(
+      () => create(recursive: any(named: 'recursive')),
+    ).thenAnswer((_) async => this);
   }
 }
