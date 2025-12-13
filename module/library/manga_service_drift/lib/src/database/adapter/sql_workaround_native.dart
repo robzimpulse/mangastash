93a9e350a791889a8b6ea6f294ca9d5f6a6fa8e2
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-12-13 22:15:13 +0700

    default target platform

--- module/library/manga_service_drift/lib/src/database/adapter/sql_workaround_native.dart
+++ module/library/manga_service_drift/lib/src/database/adapter/sql_workaround_native.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
 Future<void> sqlWorkaround() async {
-  if (Platform.isAndroid) {
+  if (defaultTargetPlatform == TargetPlatform.android) {
     await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
   }
 }
