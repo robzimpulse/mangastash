ca03f43483eb1388bdc020e4b851f55b4afdfcc5
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-12-22 10:35:52 +0700

    adjust routing class

--- module/feature/feature_browse/lib/src/route_path.dart
+++ module/feature/feature_browse/lib/src/route_path.dart
@@ -1,15 +1,32 @@
 class BrowseRoutePath {
   static const browse = '/browse_source';
-  static const browseManga = '/browse_manga/:$sourceQuery';
-  static const mangaDetail = '/browse_manga/:$sourceQuery/:$mangaIdQuery';
-  static const chapterDetail =
-      '/browse_manga/:$sourceQuery/:$mangaIdQuery/:$chapterIdQuery';
+
+  static const browseManga = '/browse_manga/${BrowsePathParam.source}';
+
+  static String get mangaDetail => [
+    'browse_manga',
+    BrowsePathParam.source,
+    BrowsePathParam.mangaId,
+  ].join('/');
+
+  static String get chapterDetail => [
+    'browse_manga',
+    BrowsePathParam.source,
+    BrowsePathParam.mangaId,
+    BrowsePathParam.chapterId,
+  ].join('/');
+
   static const chapterConfig = '/manga_chapter_config';
   static const searchParam = '/manga_search_param';
   static const addManga = '/add_manga';
+}
 
-  static const sourceQuery = 'source';
-  static const mangaIdQuery = 'mangaId';
-  static const chapterIdQuery = 'chapterId';
-  static const tagIdQuery = 'tagId';
+class BrowsePathParam {
+  static const source = ':source';
+  static const mangaId = ':mangaId';
+  static const chapterId = ':chapterId';
+}
+
+class BrowseQueryParam {
+  static const tagId = 'tagId';
 }
