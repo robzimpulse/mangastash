2017a193e177b9ee71dff2321f82516f9d9fee44
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2026-01-17 18:57:23 +0700

    add source external eval

--- module/library/entity_manga_external/lib/eval_plugin.dart
+++ module/library/entity_manga_external/lib/eval_plugin.dart
@@ -3,6 +3,7 @@ import 'package:dart_eval/dart_eval_bridge.dart';
 import 'src/chapter_scrapped.eval.dart';
 import 'src/html_document.eval.dart';
 import 'src/manga_scrapped.eval.dart';
+import 'src/source_external.eval.dart';
 
 /// [EvalPlugin] for entity_manga_external
 class EntityMangaExternalPlugin implements EvalPlugin {
@@ -12,6 +13,11 @@ class EntityMangaExternalPlugin implements EvalPlugin {
   @override
   void configureForCompile(BridgeDeclarationRegistry registry) {
     registry.defineBridgeClass($HtmlDocument.$declaration);
+    registry.defineBridgeClass($SourceExternal.$declaration);
+    registry.defineBridgeClass($GetMangaUseCase.$declaration);
+    registry.defineBridgeClass($GetChapterImageUseCase.$declaration);
+    registry.defineBridgeClass($SearchMangaExternalUseCase.$declaration);
+    registry.defineBridgeClass($SearchChapterExternalUseCase.$declaration);
     registry.defineBridgeClass($ChapterScrapped.$declaration);
     registry.defineBridgeClass($MangaScrapped.$declaration);
   }
@@ -19,6 +25,11 @@ class EntityMangaExternalPlugin implements EvalPlugin {
   @override
   void configureForRuntime(Runtime runtime) {
     $HtmlDocument.configureForRuntime(runtime);
+    $SourceExternal.configureForRuntime(runtime);
+    $GetMangaUseCase.configureForRuntime(runtime);
+    $GetChapterImageUseCase.configureForRuntime(runtime);
+    $SearchMangaExternalUseCase.configureForRuntime(runtime);
+    $SearchChapterExternalUseCase.configureForRuntime(runtime);
     $ChapterScrapped.configureForRuntime(runtime);
     $MangaScrapped.configureForRuntime(runtime);
   }
--- module/library/entity_manga_external/lib/src/source_external.dart
+++ module/library/entity_manga_external/lib/src/source_external.dart
@@ -1,9 +1,11 @@
+import 'package:eval_annotation/eval_annotation.dart';
 import 'package:manga_dex_api/manga_dex_api.dart';
 
 import 'chapter_scrapped.dart';
 import 'html_document.dart';
 import 'manga_scrapped.dart';
 
+@Bind()
 abstract class SourceExternal {
   String get name;
   String get iconUrl;
@@ -16,16 +18,19 @@ abstract class SourceExternal {
   SearchChapterExternalUseCase get searchChapterUseCase;
 }
 
+@Bind()
 abstract class GetMangaUseCase {
   List<String> get scripts;
   Future<MangaScrapped> parse({required HtmlDocument root});
 }
 
+@Bind()
 abstract class GetChapterImageUseCase {
   List<String> get scripts;
   Future<List<String>> parse({required HtmlDocument root});
 }
 
+@Bind()
 abstract class SearchMangaExternalUseCase {
   List<String> get scripts;
   String url({required SearchMangaParameter parameter});
@@ -33,6 +38,7 @@ abstract class SearchMangaExternalUseCase {
   Future<bool?> haveNextPage({required HtmlDocument root});
 }
 
+@Bind()
 abstract class SearchChapterExternalUseCase {
   List<String> get scripts;
   Future<List<ChapterScrapped>> parse({required HtmlDocument root});
