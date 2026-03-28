a58472bbf6f0b009b51c17e93446fd6799351c48
Robyarta <kugelfang.killaruna@gmail.com>
2026-03-28 10:01:45 +0700

    Adjust pipeline release (#71)
    * attach artifact to release
    
    * bump version
    
    * melos upgrade
    
    * update pod file
    
    * adjust host
    
    * add dedicated scrapped object to external library
    
    * add search manga url
    
    * adjust name
    
    * add entity manga external
    
    * fix different url
    
    * fix different url
    
    * update sqlite3 to 3.52.0 in Podfile.lock

---
+++ module/library/entity_manga_external/lib/src/source_external.dart
@@ -0,0 +1,44 @@
+import 'package:html/dom.dart';
+import 'package:manga_dex_api/manga_dex_api.dart';
+
+import 'chapter_scrapped.dart';
+import 'manga_scrapped.dart';
+
+abstract class SourceExternal {
+  String get name;
+  String get iconUrl;
+  String get baseUrl;
+
+  GetMangaUseCase get getMangaUseCase;
+  GetChapterUseCase get getChapterImageUseCase;
+  SearchMangaUseCase get searchMangaUseCase;
+  SearchChapterUseCase get searchChapterUseCase;
+
+  SearchMangaUrl get searchMangaUrl;
+}
+
+abstract class GetMangaUseCase {
+  List<String> get scripts;
+  Future<MangaScrapped> parse({required Document root});
+}
+
+abstract class GetChapterUseCase {
+  List<String> get scripts;
+  Future<List<String>> parse({required Document root});
+}
+
+abstract class SearchMangaUseCase {
+  List<String> get scripts;
+  String url({required SearchMangaParameter parameter});
+  Future<List<MangaScrapped>> parse({required Document root});
+  Future<bool?> haveNextPage({required Document root});
+}
+
+abstract class SearchChapterUseCase {
+  List<String> get scripts;
+  Future<List<ChapterScrapped>> parse({required Document root});
+}
+
+abstract class SearchMangaUrl {
+  String url({required SearchMangaParameter parameter});
+}
