2740791714ff4e7a9964121137925722c72b2f12
Robyarta Haruli Ruci <kugelfang.killaruna@gmail.com>
2026-08-08 20:38:38 +0700

    feat(domain_manga): add Isekai Scans source

--- module/domain/domain_manga/lib/src/sources/sources.dart
+++ module/domain/domain_manga/lib/src/sources/sources.dart
@@ -2,6 +2,7 @@ import 'package:collection/collection.dart';
 import 'package:entity_manga_external/entity_manga_external.dart';
 
 import 'asura_scan_source_external.dart';
+import 'isekai_scans_source_external.dart';
 import 'manga_dex_source_external.dart';
 import 'mangakatana_source_external.dart';
 import 'manganato_source_external.dart';
@@ -18,6 +19,7 @@ class Sources {
     MangakatanaSourceExternal(),
     ToonilySourceExternal(),
     ManhuaPlusSourceExternal(),
+    IsekaiScansSourceExternal(),
   ];
 
   static SourceExternal? fromName(String name) {
