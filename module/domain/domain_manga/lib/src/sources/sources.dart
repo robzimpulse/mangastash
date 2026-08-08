4a163a38c8da14f8903835f35b97a79a00b48416
Robyarta Haruli Ruci <kugelfang.killaruna@gmail.com>
2026-08-08 20:54:42 +0700

    feat(domain_manga): add Flame Comics source

--- module/domain/domain_manga/lib/src/sources/sources.dart
+++ module/domain/domain_manga/lib/src/sources/sources.dart
@@ -2,6 +2,7 @@ import 'package:collection/collection.dart';
 import 'package:entity_manga_external/entity_manga_external.dart';
 
 import 'asura_scan_source_external.dart';
+import 'flame_comics_source_external.dart';
 import 'isekai_scans_source_external.dart';
 import 'manga_dex_source_external.dart';
 import 'mangakatana_source_external.dart';
@@ -20,6 +21,7 @@ class Sources {
     ToonilySourceExternal(),
     ManhuaPlusSourceExternal(),
     IsekaiScansSourceExternal(),
+    FlameComicsSourceExternal(),
   ];
 
   static SourceExternal? fromName(String name) {
