64f9d8f980af4275e5e3fac59c1e37c9ff793947
Robyarta Haruli Ruci <kugelfang.killaruna@gmail.com>
2026-08-11 06:17:53 +0700

    refactor(domain_manga): remove Manganato source

--- module/domain/domain_manga/lib/src/sources/sources.dart
+++ module/domain/domain_manga/lib/src/sources/sources.dart
@@ -6,7 +6,6 @@ import 'flame_comics_source_external.dart';
 import 'isekai_scans_source_external.dart';
 import 'manga_dex_source_external.dart';
 import 'mangakatana_source_external.dart';
-import 'manganato_source_external.dart';
 import 'manhua_plus_source_external.dart';
 import 'toonily_source_external.dart';
 import 'weeb_central_source_external.dart';
@@ -16,7 +15,6 @@ class Sources {
     MangaDexSourceExternal(),
     AsuraScanSourceExternal(),
     WeebCentralSourceExternal(),
-    ManganatoSourceExternal(),
     MangakatanaSourceExternal(),
     ToonilySourceExternal(),
     ManhuaPlusSourceExternal(),
